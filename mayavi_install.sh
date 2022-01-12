93b01a3ebe32054f40288ff36d697a4dac44051b
hahnec <christopher.hahne@unibe.ch>
2022-01-12 16:35:15 +0100

    refactor(sh): remove mayavi_install.sh script (replaced by install_env.sh)

--- mayavi_install.sh
+++
@@ -1,9 +0,0 @@
-source env/bin/activate
-
-python -m pip install vtk==9.0.1
-python -m pip install PyQt5
-python -m pip install numpy
-
-git clone https://github.com/enthought/mayavi.git
-cd mayavi
-python setup.py install
