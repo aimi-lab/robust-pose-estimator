7cd1f8356d413c65a803f4a18687f199adaf35da
hahnec <christopher.hahne@unibe.ch>
2022-01-18 14:08:14 +0100

    refactor(rect): changes after testing rectification impact

--- scripts/gt_coords_comparison.py
+++ scripts/gt_coords_comparison.py
@@ -2,13 +2,11 @@ import numpy as np
 import imageio
 import pickle
 from skimage import transform, util
-from mayavi import mlab
-from pathlib import Path
 import json
 from tifffile import tifffile
 
 from alley_oop.utils.paths import SCARED_ROOT_PATH, get_scared_abspath
-from alley_oop.utils.pinhole_transforms import reverse_project, forward_project, create_img_coords
+from alley_oop.utils.pinhole_transforms import reverse_project, create_img_coords
 from alley_oop.utils.pfm_handler import load_pfm
 
 
@@ -28,27 +26,23 @@ def rescale_intrinsics(kmat, origin_size=None, target_size=None):
 
     return kmat
 
-def load_tiff_pcl(d_idx:int=1, k_idx:int=1):
+def load_tiff_pcl(d_idx:int=1, k_idx:int=1, fname=None):
 
-    from alley_oop.utils.paths import get_scared_abspath
+    if fname is None: fname = get_scared_abspath(d_idx, k_idx, 'left_depth_map.tiff')
 
-    tpcls = []
-    for fname in ['left_depth_map.tiff', 'right_depth_map.tiff']:
+    # tiff-based depth
+    tpcl = np.array(tifffile.imread(fname))
 
-        # tiff-based depth
-        tpcl = np.array(tifffile.imread(get_scared_abspath(d_idx, k_idx, fname=fname)))
+    return tpcl
 
-        tpcls.append(tpcl)
-
-    return tpcls
-
-depth_path = SCARED_ROOT_PATH.parent / 'generated_depth_log_1641508997'
+depth_path = SCARED_ROOT_PATH.parent / 'generated_depth_log_1642502310'#'generated_depth_log_1642501697'#'generated_depth_log_1641508997'#'generated_depth_log_1642441959'#'generated_depth_log_1642442355'#
 depth_list = sorted(depth_path.rglob('*d_*.pfm'))
 
 calib_list = []
 fnimg_list = []
 ipair_path = get_scared_abspath(d_idx=1, k_idx=1)
 ipair_list = sorted((ipair_path / 'data' / 'video_frames').rglob('*.*'))
+scene_list = sorted((ipair_path / 'data' / 'scene_points').rglob('*.*'))
 calib_list += ipair_list[0::3]
 fnimg_list += ipair_list[1::3]
 
@@ -57,14 +51,15 @@ i = 0
 dis0 = load_pfm(depth_list[i])[0]
 img0 = imageio.imread(fnimg_list[i])
 with open(calib_list[i],'rb') as f: cal0 = pickle.load(f)
-pclg = load_tiff_pcl(d_idx=1, k_idx=1)[0]
+pclg = load_tiff_pcl()[:1024, ...]#fname=scene_list[i]
 
 # prepare data
-ds = 2
-resolution = (512*ds, 640*ds)
+resolution = (1024, 1280)
+us = .5
+ds = int(2/us)
 cal0['M1'] = rescale_intrinsics(cal0['M1'], origin_size=img0.shape[:2], target_size=resolution)
 dis0 = clip_quantile(dis0)
-dis0 = transform.resize(dis0, resolution)/(dis0.shape[1]/1280)
+dis0 = transform.resize(dis0, resolution)/(dis0.shape[1]/resolution[1])
 img0 = util.img_as_ubyte(transform.resize(img0, resolution))
 pclg = pclg.reshape(-1, 3).T
 
@@ -85,8 +80,8 @@ opts = reverse_project(ipts, cal0['M1'], disp=dis0.flatten(), base=bas0)
 #opts = rmat @ opts + tvec
 
 # select points where ground-truth reference is available (exclude NaNs)
-pcln = pclg[:, ~np.isnan(pclg.sum(0))]
-epts = opts[:, ~np.isnan(pclg.sum(0))]
+pcln = pclg[:, (~np.isnan(pclg.sum(0)))]
+epts = opts[:, (~np.isnan(pclg.sum(0)))]
 
 # numerical displacement errors
 abs_dev_x = np.mean((epts[0] - pcln[0])**2)**.5
@@ -110,5 +105,5 @@ ds = 500
 ax.plot(*orgn, marker='s', color='orange', markersize=6, linestyle='none', label='keyframe')
 ax.plot(epts[0, ::ds], epts[1, ::ds], epts[2, ::ds], 's', color='b', markersize=3, label='estimated points')
 ax.plot(pcln[0, ::ds], pcln[1, ::ds], pcln[2, ::ds], 'o', color='g', markersize=3, label='ground-truth points')
-plt.legend()
+plt.legend(loc='upper right')
 plt.show()
