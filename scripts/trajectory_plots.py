84c1a3a1b074df6670eef9bea455b982dda15940
hahnec <christopher.hahne@unibe.ch>
2022-01-20 15:54:20 +0100

    fix(parser): move parser to main script

--- scripts/trajectory_plots.py
+++ scripts/trajectory_plots.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     d_idx = 1
 
     # iterate through keyframes
-    for k_idx in range(3, 4):
+    for k_idx in range(1, 4):
         pose_plotter = TrajectoryAnalyzer(title='dataset_'+str(d_idx)+', keyframe_'+str(k_idx))
         for k, meth in enumerate(meth_dirs):
             pose_arrs = load_scared_pose(d_idx=d_idx, k_idx=k_idx, meth=meth)
