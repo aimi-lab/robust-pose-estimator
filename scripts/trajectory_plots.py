dd8f59e9bb539cab03276482ee14810b735998cf
mhayoz <mi.hayoz@sunrise.ch>
2022-02-02 14:33:31 +0100

    support elastic fusion

--- scripts/trajectory_plots.py
+++ scripts/trajectory_plots.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 import json
 import argparse
+from scipy.spatial.transform import Rotation
 
 from alley_oop.utils.paths import get_scared_abspath
 from alley_oop.pose.trajectory_analyzer import TrajectoryAnalyzer
@@ -30,6 +31,26 @@ def load_scared_pose(d_idx:int=1, k_idx:int=1, meth='frame_data') -> np.ndarray:
     return pose_arrs
 
 
+def load_elastic_fusion_pose(meth, scale=20.0):
+    trajectory_file = list(Path(meth).glob('*.klg.freiburg'))
+    assert len(trajectory_file) == 1
+    with open(str(trajectory_file[0]), 'r') as f: pose_list = np.loadtxt(f)
+    pose_arrs = []
+    time_stamps = []
+    for p in pose_list:
+        rel_pose_arr = np.eye(4)
+        rel_pose_arr[:3,3] = scale*p[1:4] # translation
+        quaternion = Rotation.from_quat(p[4:8])
+        rel_pose_arr[:3,:3] = quaternion.as_matrix()
+        time_stamps.append(p[0])
+        pose_arrs.append(np.linalg.pinv(rel_pose_arr))
+
+    pose_arrs = np.asarray(pose_arrs)
+    time_stamps = np.asarray(time_stamps)
+
+    return pose_arrs
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='ORB SLAM example')
@@ -42,16 +63,17 @@ if __name__ == '__main__':
     parser.add_argument(
         '--pred_folder',
         type=str,
-        default='orbslam2_rgbd_results',
+        nargs='+',
+        default=['orbslam2_rgbd_results'],
         help='Folder containing predictions.'
     )
 
     try:
         args = parser.parse_args()
-        assert (Path(args.base_path) / 'frame_data').exists()
-        assert (Path(args.base_path) / args.pred_folder).exists()
-        data_path = Path(args.base_path) / args.pred_folder / args.base_path
-        meth_dirs = [str(data_path / 'frame_data')]
+        meth_dirs = []
+        for pred_folder in args.pred_folder:
+            assert (Path(args.base_path) / pred_folder).exists()
+            meth_dirs += [str(Path(args.base_path) / pred_folder)]
     except:
         meth_dirs = ['orbslam2_stereo_results', 'frame_data'] #, 'orbslam2_rgbd_results'
     
@@ -62,7 +84,11 @@ if __name__ == '__main__':
     for k_idx in range(1, 4):
         pose_plotter = TrajectoryAnalyzer(title='dataset_'+str(d_idx)+', keyframe_'+str(k_idx))
         for k, meth in enumerate(meth_dirs):
-            pose_arrs = load_scared_pose(d_idx=d_idx, k_idx=k_idx, meth=meth)
+            if 'elastic_fusion' in meth:
+                pose_arrs = load_elastic_fusion_pose(meth)
+            else:
+                pose_arrs = load_scared_pose(d_idx=d_idx, k_idx=k_idx, meth=meth)
+            print(len(pose_arrs))
             color = colors[k%len(colors)]
             pose_plotter.add_pose_trajectory(pose_arrs, label=meth, color=color)
         pose_plotter.legend()
