cd636cf8a1d36d1c99f0a0fe23b2d223de81a152
hahnec <christopher.hahne@unibe.ch>
2022-02-08 09:46:41 +0100

    feat(tests): add tests for pose regression and quaternions

--- tests/unit_test_pose.py
+++ tests/unit_test_pose.py
@@ -1,7 +1,9 @@
 import unittest
 import numpy as np
+import matplotlib.pyplot as plt
 
 from alley_oop.pose.feat_pose_estimation import FeatPoseEstimator
+from alley_oop.pose.quaternions import quat2rmat, euler2quat
 
 
 class PoseTester(unittest.TestCase):
@@ -10,30 +12,43 @@ class PoseTester(unittest.TestCase):
         super(PoseTester, self).__init__(*args, **kwargs)
 
     def setUp(self):
-        pass
+
+        self.plt_opt = False
     
     def test_feat_pose(self):
         
         # synthetic points
-        refer = np.array([[0,0,0], [1,0,0], [0,0,1], [1,1,1], [1,1,0], [1,0,1], [0,1,1], [10,4,1], [100,100,100]]).T
+        refer = np.array([[0,0,0], [1,0,0], [0,0,1], [1,1,1], [1,1,0], [1,0,1], [0,1,1], [10,4,1], [10,10,10]]).T*10
+        refer += 100
 
-        # translation and rotation definition
-        from scipy.spatial.transform import Rotation as R
-        r_true = R.from_euler('z', 30, degrees=True).as_matrix()
-        t_true = np.array([1, 1, 1])[:,None]
+        for a in np.arange(0, 90, 2):
+            # translation and rotation definition
+            from scipy.spatial.transform import Rotation as R
+            r_true = R.from_euler('z', a, degrees=True).as_matrix()
+            t_true = np.array([1, 1, 1])[:, None]
+            t_true *= a if a != 0 else 1
 
-        # transform points
-        query = r_true @ refer + t_true
+            # transform points
+            query = r_true @ refer + t_true
 
-        # estimate translation and rotation
-        estimator = FeatPoseEstimator(feat_refer=refer, feat_query=query)
-        estimator.estimate()
+            # estimate translation and rotation
+            estimator = FeatPoseEstimator(feat_refer=refer, feat_query=query, quat_opt=False)
+            estimator.estimate()
+            solve = estimator.rmat @ refer + estimator.tvec
 
-        # assert output estimates
-        t_bool = np.allclose(t_true, estimator.tvec, atol=1e-3)
-        r_bool = np.allclose(r_true, estimator.rmat, atol=1e-3)
-        self.assertTrue(t_bool)
-        self.assertTrue(r_bool)
+            if self.plt_opt:
+                fig = plt.figure(figsize=(12, 12))
+                ax = fig.add_subplot(projection='3d')
+                ax.scatter(refer[0], refer[1], refer[2], 'bx')
+                ax.scatter(query[0], query[1], query[2], 'r.')
+                ax.scatter(solve[0], solve[1], solve[2], 'go')
+                plt.show()
+
+            # assert output estimates
+            t_bool = np.allclose(np.round(t_true, 3), np.round(estimator.tvec, 3), atol=1e-3)
+            r_bool = np.allclose(np.round(r_true, 2), np.round(estimator.rmat, 2), atol=1e-3)
+            self.assertTrue(t_bool, msg='failed for angle %s' % a)
+            self.assertTrue(r_bool, msg='failed for angle %s' % a)
 
     def test_all(self):
 
