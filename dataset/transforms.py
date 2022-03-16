83f20a7c63367d620082e764b56ac11f6e199fba
mhayoz <mi.hayoz@sunrise.ch>
2022-03-16 08:30:39 +0100

    add video dataset

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -29,3 +29,30 @@ class ResizeRGBD(RGBDTransform):
             label = cv2.resize(label, (int(w * scale), int(h * scale)), cv2.INTER_NEAREST)
             label = label[int(crop[1] / 2):int(h * scale - crop[1] / 2), int(crop[0] / 2):int(w * scale - crop[0] / 2)]
         return img, disparity, mask, label
+
+
+class StereoTransform(object):
+    def __call__(self, left, depth, mask=None):
+        return left, depth, mask
+
+
+class ResizeStereo(StereoTransform):
+    def __init__(self, size):
+        self.size = int(size[0]), int(size[1])
+
+    def __call__(self, left_img, right_img, label=None):
+        # resize with cropping to conserve aspect ratio
+        h, w = left_img.shape[:2]
+        scale = max(self.size[0] / w, self.size[1] / h)
+        crop = w * scale - self.size[0], h * scale - self.size[1]
+        left_img = cv2.resize(left_img, (int(w * scale), int(h * scale)))
+        left_img = left_img[int(crop[1] / 2):int(h * scale - crop[1] / 2), int(crop[0] / 2):int(w * scale - crop[0] / 2)]
+        right_img = cv2.resize(left_img, (int(w * scale), int(h * scale)))
+        right_img = right_img[int(crop[1] / 2):int(h * scale - crop[1] / 2),
+                   int(crop[0] / 2):int(w * scale - crop[0] / 2)]
+        if label is not None:
+            label = cv2.resize(label, (int(w * scale), int(h * scale)), cv2.INTER_NEAREST)
+            label = label[int(crop[1] / 2):int(h * scale - crop[1] / 2), int(crop[0] / 2):int(w * scale - crop[0] / 2)]
+            return left_img, right_img, label
+        else:
+            return left_img, right_img
