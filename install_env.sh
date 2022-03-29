0bb3e069faee9bc5ecbd0fbdf7a74138f6726bb9
hahnec <christopher.hahne@unibe.ch>
2022-03-29 16:32:59 +0200

    feat(req): add requirements from submodule

--- install_env.sh
+++ install_env.sh
@@ -2,6 +2,7 @@ python3 -m venv env
 source env/bin/activate
 
 python3 -m pip install -r requirements.txt
+python3 -m pip install -r torchimize/requirements.txt
 
 git clone https://github.com/enthought/mayavi.git
 cd mayavi
