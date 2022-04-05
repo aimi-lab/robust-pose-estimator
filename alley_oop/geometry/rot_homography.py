fixed some issues with jacobians but still not converging

alley_oop/geometry/rot_homography.py
@@ -2,7 +2,7 @@ import torch
 from torch.nn.functional import conv2d
 import numpy as np
 from torchgeometry import homography_warp
-from alley_oop.geometry.lie_3d import lie_alebra2group_rot
+from alley_oop.geometry.lie_3d import lie_alebra2group_rot, lie_hatmap
 
 """ this is an implementation of  
     https://www.robots.ox.ac.uk/~cmei/articles/omni_track_mei.pdf
@@ -25,48 +25,55 @@ def image_jacobian(img):
     return jacobian
 
 
-def proj_jacobian(x,y):
-    """ we combine Jpi, Jw and JH(0) in a single matrix 2x8, this can be found by multiplying the individual matrices in
-        https://www.robots.ox.ac.uk/~cmei/articles/single_view_track_ITRO.pdf.
-        Note that we set the focal length f=1 since it is a common factor and let Zs=1 for the image plane at 1,
-        we adapt J(H0) for the special case of rotation with three skew-symmetric matrices as generators for so(3)
 
-        this leads to [xy         -1-x**2       y
-                       -1+y**2     -xy          x]"""
+def batch_jw(img, K):
+    """ jacobian of action w with respect to R(0)
+        w<KR(x)K'><(u v 1)T>
+        adapted from https://www.robots.ox.ac.uk/~cmei/articles/single_view_track_ITRO.pdf where H(x) = K R(x) K'
+        """
+    assert K.shape == (3,3)
+    u, v = torch.meshgrid(torch.arange(img.shape[-1]), torch.arange(img.shape[-2]))
+    u = u.T.reshape(-1)
+    v = v.T.reshape(-1)
 
-    return torch.tensor([[x*y,-1-x**2, y],
-                        [-1+y**2, -x*y, x]])
-
-
-def batch_proj_jacobian(img):
-    """ we combine Jpi, Jw and JH(0) in a single matrix 2x8, this can be found by multiplying the individual matrices in
-        https://www.robots.ox.ac.uk/~cmei/articles/single_view_track_ITRO.pdf.
-        Note that we set the focal length f=1 since it is a common factor and let Zs=1 for the image plane at 1,
-        we adapt J(H0) for the special case of rotation with three skew-symmetric matrices as generators for so(3)
-
-        this leads to [xy         -1-x**2       y
-                       1+y**2     -xy          -x]"""
-    xs, ys = torch.meshgrid(torch.arange(img.shape[-1]), torch.arange(img.shape[-2]))
-    xs = xs.reshape(-1)
-    ys = ys.reshape(-1)
-    # J = torch.empty((img.shape[-1]*img.shape[-2], 2, 3))
-    # # slow loop
-    # for i, (x,y) in enumerate(zip(xs, ys)):
-    #     J[i] = proj_jacobian(x,y)
+    # intrinsics
+    cu = K[0,2]
+    cv = K[1,2]
+    f = K[0,0]
 
     # fast
-    J2 = torch.empty((img.shape[-1] * img.shape[-2], 2, 3))
-    J2[:, 0, 0] = xs*ys
-    J2[:, 0, 1] = -xs*xs-1
-    J2[:, 0, 2] = ys
-    J2[:, 1, 0] = ys*ys+1
-    J2[:, 1, 1] = -xs*ys
-    J2[:, 1, 2] = -xs
+    J2 = torch.zeros((img.shape[-1] * img.shape[-2], 3, 9))
+    J2[:, 0, 0] = u -cu
+    J2[:, 0, 1] = v -cv
+    J2[:, 0, 2] = f
+
+    J2[:, 0, 6] = -1/f*(u-cu)**2
+    J2[:, 0, 7] = -1/f*(u-cu)*(v-cv)
+    J2[:, 0, 8] = -(u -cu)
+
+    J2[:, 1, 3] = u -cu
+    J2[:, 1, 4] = v -cv
+    J2[:, 1, 5] = f
+
+    J2[:, 1, 6] = -1/f*(u-cu)*(v-cv)
+    J2[:, 1, 7] = -1/f*(v-cv)**2
+    J2[:, 1, 8] = -(v-cv)
 
     return J2
 
 
-def ems_jacobian(img1, img2, batch_proj_jac=None):
+def j_rot():
+    """ jacobian of R(x) wrt. x -> generators of so(3) in vector form
+        adapted from https://www.robots.ox.ac.uk/~cmei/articles/single_view_track_ITRO.pdf
+        """
+    J = torch.empty((9,3))
+    J[:, 0] = torch.tensor(lie_hatmap(np.array([1,0,0]))).reshape(-1)
+    J[:, 1] = torch.tensor(lie_hatmap(np.array([0,1,0]))).reshape(-1)
+    J[:, 2] = torch.tensor(lie_hatmap(np.array([0,0,1]))).reshape(-1)
+    return J
+
+
+def ems_jacobian(img1, img2, K, batch_proj_jac=None):
     """ Jacobian for efficient least squares (Jimg1 + Jimg2)/2 * Jproj in R^(h*w)x2"""
     assert img1.ndim == 2
     assert img2.ndim == 2
@@ -79,9 +86,9 @@ def ems_jacobian(img1, img2, batch_proj_jac=None):
     J_img = (J_img[0] + J_img[1])/2
     J_img = J_img.reshape(h*w,1,2).squeeze(0)
     if batch_proj_jac is None:
-        batch_proj_jac = batch_proj_jacobian(img1)
+        batch_proj_jac = (batch_jw(img1, K) @ j_rot())[:, :2] # remove third line (zeros because we don't use w coordinates)
     J = J_img @ batch_proj_jac
-    return J.squeeze(1)
+    return J.squeeze(1), batch_proj_jac
 
 
 def so3(x):
@@ -94,20 +101,22 @@ def warp_img(img, R, K):
 
 
 def efficient_least_squares(img1, img2, K, n_iter=1000, res_thr=0.00001):
-    proj_jac = batch_proj_jacobian(img1)
 
     R_lr = torch.eye(3).unsqueeze(0)
+    J_proj = None
     for i in range(n_iter):
         # compute residuals f(x)
         warped_img = warp_img(img1, R_lr, K)
-        J = ems_jacobian(warped_img, img2, proj_jac)
+        J, J_proj = ems_jacobian(warped_img, img2, K, J_proj)
         J_pinv = torch.linalg.pinv(J)
-        residuals = (0.5*(warped_img - img2)**2).view(-1, 1)
+        residuals = (0.5*(warped_img - img2)**2).reshape(-1, 1)
         # compute update parameter x0
         x0 = -J_pinv @ residuals
 
         # update rotation estimate
         R_lr = R_lr @ so3(x0)
+        print(x0)
+        print(residuals.mean())
         plot(warped_img, img2)
 
         if residuals.mean() < res_thr:
@@ -118,8 +127,8 @@ from scipy.spatial.transform import Rotation
 import cv2
 img1 = torch.tensor(cv2.resize(cv2.imread('../../tests/test_data/000000l.png', cv2.IMREAD_GRAYSCALE), (160, 128))).float()
 f=1200.0
-cx=60
-cy=50
+cx=79.5
+cy=63.5
 intrinsics = torch.tensor([[f, 0, cx], [0, f, cy], [0,0,1.0]]).float()
 R_true = torch.tensor(Rotation.from_euler('z', 3.0, degrees=True).as_matrix()).float()
 img2cv = warp_img(img1, R_true, intrinsics)