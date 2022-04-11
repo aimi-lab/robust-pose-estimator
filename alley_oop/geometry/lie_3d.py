5e5ca7a2198af7903317e0705db4d0c1a5393540
hahnec <christopher.hahne@unibe.ch>
2022-04-11 12:44:32 +0200

    fix(se3): rectify se3/SE3 Lie funs and add tests

--- alley_oop/geometry/lie_3d.py
+++ alley_oop/geometry/lie_3d.py
@@ -11,12 +11,12 @@ from alley_oop.utils.lib_handling import get_lib_type, get_data_class
 
 def lie_so3_to_SO3(
     wvec: Union[np.ndarray, torch.Tensor] = None,
-    tol: float = 10e-12
+    tol: float = 10e-12,
     ) -> Union[np.ndarray, torch.Tensor]:
     """ 
-    create rotation matrix SO(3) from Euler vector
+    create rotation matrix in SO(3) from Lie angle 3-vector
     
-    :param wvec: Euler vector
+    :param wvec: Lie angle 3-vector
     :return: rotation matrix in SO(3)
     """
 
@@ -36,19 +36,19 @@ def lie_so3_to_SO3(
     wmat = lie_hatmap(wvec)
 
     # compute exponential of hat-map using Taylor expansion (known as Rodrigues formula)
-    rmat = lib.eye(3) + wmat * lib.sin(theta) + wmat @ wmat *(1-lib.cos(theta))
+    rmat = lib.eye(3) + lib.sin(theta) * wmat + (1-lib.cos(theta)) * wmat @ wmat
 
     return rmat
 
 
 def lie_SO3_to_so3(
-        rmat: Union[np.ndarray, torch.Tensor] = None
+        rmat: Union[np.ndarray, torch.Tensor] = None,
     ) -> Union[np.ndarray, torch.Tensor]:
     """ 
-    create Euler vector from rotation matrix in SO(3)
+    create Lie angle 3-vector from rotation matrix in SO(3)
     
     :param rmat: rotation matrix in SO(3)
-    :return: Euler vector
+    :return: Lie angle 3-vector
     """
 
     lib = get_lib_type(rmat)
@@ -74,61 +74,84 @@ def lie_SO3_to_so3(
     return wvec
 
 
-def lie_SE3_to_se3(
-        rmat: Union[np.ndarray, torch.Tensor] = None, 
-        tvec: Union[np.ndarray, torch.Tensor] = None
-    ) -> Union[np.ndarray, torch.Tensor]:
-    """
-    caution: not tested 
-    tbd: catch cases where theta=0
-    """
-
-    lib = get_lib_type(rmat)
-
-    wvec = lie_SO3_to_so3(rmat)
-    wmat = lie_hatmap(wvec)
-
-    theta = (wvec.T @ wvec)**.5
-    a_term = lib.sin(theta) / theta
-    b_term = (1-lib.cos(theta)) / theta**2
-    #c_term = (1-a_term) / theta**2
-
-    vmat_inv = lib.eye(3) - .5*wmat + 1/theta**2*(1-a_term/(2*b_term)) * wmat @ wmat
-
-    uvec = vmat_inv @ tvec
-
-    return wvec, uvec
-
-
 def lie_se3_to_SE3(
         wvec: Union[np.ndarray, torch.Tensor] = None,
-        uvec: Union[np.ndarray, torch.Tensor] = None
+        uvec: Union[np.ndarray, torch.Tensor] = None,
+        tol: float = 10e-12,
     ) -> Union[np.ndarray, torch.Tensor]:
     """
-    caution: not tested 
-    tbd: catch cases where theta=0
+    create rotation matrix in SO(3) and translation vector R^3 from Lie algebra equivalents
+    
+    :param wvec: Lie angle 3-vector
+    :param uvec: Lie translation 3-vector
+    :return: rotation matrix in SO(3), translation vector R^3
     """
 
     lib = get_lib_type(wvec)
 
-    wmat = lie_hatmap(wvec)
-    rmat = lie_so3_to_SO3(wvec)
-
+    # compute scale from vector norm
     theta = (wvec.T @ wvec)**.5
-    a_term = lib.sin(theta) / theta
-    b_term = (1-lib.cos(theta)) / theta**2
-    c_term = (1-a_term) / theta**2
+
+    # Taylor coefficients
+    a_term = lib.sin(theta) #/ theta
+    b_term = (1-lib.cos(theta)) #/ theta**2
+    c_term = (1-a_term) #/ theta**2
+
+    # normalize vector
+    wvec_norm = wvec / theta if theta > tol else wvec
+
+    # construct hat-map which is so(3)
+    wmat = lie_hatmap(wvec_norm)
     
-    #rmat = np.eye(3) - a_term*wmat + b_term*wmat**2
-    vmat = lib.eye(3) - b_term*wmat + c_term*wmat @ wmat
+    rmat = lib.eye(3) + a_term * wmat + b_term * wmat @ wmat
+    vmat = lib.eye(3) + b_term * wmat + c_term * wmat @ wmat
 
     tvec = vmat @ uvec
 
     return rmat, tvec
 
 
+def lie_SE3_to_se3(
+        rmat: Union[np.ndarray, torch.Tensor] = None, 
+        tvec: Union[np.ndarray, torch.Tensor] = None,
+        tol: float = 10e-12,
+    ) -> Union[np.ndarray, torch.Tensor]:
+    """
+    create rotation matrix in SO(3) and translation vector R^3 from Lie algebra equivalents
+    
+    :param rmat: rotation matrix in SO(3)
+    :param tvec: translation vector R^3
+    :return: Lie angle 3-vector, Lie translation 3-vector
+    """
+
+    lib = get_lib_type(rmat)
+
+    wvec = lie_SO3_to_so3(rmat)
+
+    # compute scale from vector norm
+    theta = (wvec.T @ wvec)**.5
+
+    # Taylor coefficients
+    a_term = lib.sin(theta)
+    b_term = (1-lib.cos(theta))
+
+    # normalize vector
+    wvec_norm = wvec / theta if theta > tol else wvec
+
+    # construct hat-map which is so(3)
+    wmat = lie_hatmap(wvec_norm)
+
+    if b_term != 0:
+        vmat_inv = lib.eye(3) - .5*wmat + (1-a_term/(2*b_term)) * wmat @ wmat
+        uvec = vmat_inv @ tvec
+    else:
+        uvec = lib.zeros(3)
+
+    return wvec, uvec
+
+
 def lie_hatmap(
-        wvec: Union[np.ndarray, torch.Tensor] = None
+        wvec: Union[np.ndarray, torch.Tensor] = None,
     ) -> Union[np.ndarray, torch.Tensor]:
     """ 
     create hat-map in so(3) from Euler vector in R^3
--- tests/unit_test_lie.py
+++ tests/unit_test_lie.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 import torch
 
-from alley_oop.geometry.lie_3d import lie_so3_to_SO3, lie_SO3_to_so3, is_SO3
+from alley_oop.geometry.lie_3d import lie_so3_to_SO3, lie_SO3_to_so3, is_SO3, lie_SE3_to_se3, lie_se3_to_SE3
 
 class Lie3DTester(unittest.TestCase):
 
@@ -14,47 +14,112 @@ class Lie3DTester(unittest.TestCase):
         np.random.seed(3008)
         torch.manual_seed(3008)
 
-    def test_lie_conversion(self):
+    def test_so3_conversions(self):
 
         arr = .25 * np.random.randn(100, 3)
 
         for p in arr:
             
             # convert 3-vector to rotation matrix
-            qs = lie_so3_to_SO3(p)
+            rmat = lie_so3_to_SO3(p)
 
             # check if rotation matrix is SO3
-            rval = is_SO3(qs)
+            rval = is_SO3(rmat)
             self.assertTrue(rval)
 
             # convert rotation matrix to 3-vector
-            rs = lie_SO3_to_so3(qs)
+            rvec = lie_SO3_to_so3(rmat)
 
             # assertion
-            self.assertTrue(np.allclose(p, rs, atol=10e-11))
+            self.assertTrue(np.allclose(p, rvec, atol=10e-11))
 
-    def test_lie_conversion_torch(self):
+    def test_so3_conversions_torch(self):
 
         arr = .25 * torch.randn(100, 3, dtype=torch.float64)
 
         for p in arr:
             
             # convert 3-vector to rotation matrix
-            qs = lie_so3_to_SO3(p)
+            rmat = lie_so3_to_SO3(p)
 
             # check if rotation matrix is SO3
-            rval = is_SO3(qs)
+            rval = is_SO3(rmat)
             self.assertTrue(rval)
 
             # convert rotation matrix to 3-vector
-            rs = lie_SO3_to_so3(qs)
+            rvec = lie_SO3_to_so3(rmat)
 
             # assertion
-            self.assertTrue(torch.allclose(p, rs, atol=10e-11))
+            self.assertTrue(torch.allclose(p, rvec, atol=10e-11))
+
+    def test_se3_conversions(self):
+
+        arr = .25 * np.random.randn(100, 6)
+
+        for p in arr:
+            
+            # convert 3-vector to rotation matrix
+            rmat, tvec = lie_se3_to_SE3(wvec=p[:3], uvec=p[3:])
+
+            # check if rotation matrix is SO3
+            rval = is_SO3(rmat)
+            self.assertTrue(rval)
+
+            # convert rotation matrix to 3-vector
+            wvec, uvec = lie_SE3_to_se3(rmat, tvec)
+
+            # assertion
+            self.assertTrue(np.allclose(p[:3], wvec, atol=10e-11))
+            self.assertTrue(np.allclose(p[3:], uvec, atol=10e-11))
+
+    def test_se3_conversions_torch(self):
+
+        arr = .25 * torch.randn(100, 6, dtype=torch.float64)
+
+        for p in arr:
+            
+            # convert 3-vector to rotation matrix
+            rmat, tvec = lie_se3_to_SE3(wvec=p[:3], uvec=p[3:])
+
+            # check if rotation matrix is SO3
+            rval = is_SO3(rmat)
+            self.assertTrue(rval)
+
+            # convert rotation matrix to 3-vector
+            wvec, uvec = lie_SE3_to_se3(rmat, tvec)
+
+            # assertion
+            self.assertTrue(np.allclose(p[:3], wvec, atol=10e-11))
+            self.assertTrue(np.allclose(p[3:], uvec, atol=10e-11))
+
+    def test_zero_so3(self):
+
+        rmat = lie_so3_to_SO3(np.zeros(3))
+
+        self.assertTrue(np.sum(rmat - np.eye(3)) == 0, 'Zero Euler angles do not yield identity matrix')
+
+        rmat, tvec = lie_se3_to_SE3(wvec=np.zeros(3), uvec=np.zeros(3))
+
+        self.assertTrue(np.sum((rmat - np.eye(3))**2) == 0, 'Zeros in Lie angle 3-vector do not yield identity matrix')
+        self.assertTrue(np.sum(tvec**2) == 0, 'Zeros in Lie translation 3-vector are not zeros')
+
+    def test_zero_SO3(self):
+
+        rvec = lie_SO3_to_so3(rmat=np.eye(3))
+
+        self.assertFalse(rvec.any(), 'Identity rotation matrix does not yield zero vector')
+
+        rvec, uvec = lie_SE3_to_se3(rmat=np.eye(3), tvec=np.zeros(3))
+
+        self.assertFalse(rvec.any(), 'Identity rotation matrix does not yield zero vector')
+        self.assertFalse(uvec.any(), 'Zeros in translation 3-vector do not yield zeros')
 
     def test_all(self):
 
-        self.test_lie_conversion()
+        self.test_so3_conversions()
+        self.test_so3_conversions_torch()
+        self.test_se3_conversions()
+        self.test_se3_conversions_torch()
 
 
 if __name__ == '__main__':
