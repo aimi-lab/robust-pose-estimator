c3e7e7ae5d38b75524dcc6c6b739db3e794c6e0c
mhayoz <mi.hayoz@sunrise.ch>
2022-04-22 14:40:47 +0200

    flying things for unit test

--- tests/unit_test_icp.py
+++ tests/unit_test_icp.py
@@ -8,6 +8,7 @@ import torch
 from scipy.spatial.transform import Rotation as R
 from alley_oop.geometry.lie_3d import lie_SE3_to_se3
 from alley_oop.geometry.point_cloud import PointCloud
+from alley_oop.utils.pfm_handler import load_pfm
 
 class RotEstimatorTester(unittest.TestCase):
 
@@ -22,25 +23,30 @@ class RotEstimatorTester(unittest.TestCase):
 
         # load test data
         scale = 16
-        disparity = cv2.imread(str(Path.cwd() / 'tests' / 'test_data' / '000000l.pfm'), cv2.IMREAD_UNCHANGED)
-        disparity = cv2.resize(disparity, (int(disparity.shape[1]/scale), int(disparity.shape[0]/scale)))/scale
-        depth = torch.tensor(4289.756 / disparity/2).double()
+        disparity, _ = load_pfm(str(Path.cwd() / 'tests' / 'test_data' / '0006.pfm'))
+        h, w = (int(disparity.shape[0] / scale), int(disparity.shape[1] / scale))
+        disparity = cv2.resize(disparity, (w, h))
+        # background is very far, make it appear closer
+        rand_background = 17+ 6*np.random.rand(int((disparity < 20).sum()))
+        disparity[disparity < 20] = rand_background
+
+        depth = torch.tensor(1050.0 / disparity).double()
         # generate dummy intrinsics and dummy images
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-        R_true = torch.tensor(R.from_euler('xyz', (0.0, 0.0, 2), degrees=True).as_matrix()).double()
-        t_true = torch.tensor([0, 0.0, 1.0]).double()
+        R_true = torch.tensor(R.from_euler('xyz', (0.0, 1.0, 2.0), degrees=True).as_matrix()).double()
+        t_true = torch.tensor([0, 0.0, 3.0]).double()
         T_true = torch.eye(4).double()
         T_true[:3, :3] = R_true
         T_true[:3, 3] = t_true
-        intrinsics = torch.tensor([[1035.3/scale, 0, 596.955/scale],
-                                   [0, 1035.3/scale, 488.41/scale],
+        intrinsics = torch.tensor([[1050.0/scale, 0, 479.5/scale],
+                                   [0, 1050.0/scale, 269.5/scale],
                                    [0, 0, 1]]).double()
         ref_pcl = PointCloud()
         ref_pcl.from_depth(depth, intrinsics)
         target_pcl = ref_pcl.transform_cpy(T_true)
 
-        estimator = ICPEstimator(depth.shape[:2], intrinsics, Ftol=1e-1, association_mode='projective').to(device)
+        estimator = ICPEstimator(depth.shape[:2], intrinsics, Ftol=1e-3, dist_thr=5, association_mode='projective').to(device)
         with torch.no_grad():
             T, cost = estimator.estimate_lm(depth.to(device), target_pcl.to(device))
 
