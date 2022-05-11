6164b04d2f105e8694e5fc9e17a5e5702b7da3c7
mhayoz <mi.hayoz@sunrise.ch>
2022-05-11 15:51:02 +0200

    uniform color for depth

--- scripts/alleyoop_get_trajectory.py
+++ scripts/alleyoop_get_trajectory.py
@@ -79,6 +79,7 @@ def main(input_path, output_path, config, force_cpu, nsamples):
             pose, scene = slam.processFrame(limg, depth)
             if viewer is not None:
                 curr_pcl = SurfelMap(frame=slam.get_frame(), kmat=torch.tensor(calib['intrinsics']['left']).float(), pmat=pose).pcl2open3d(stable=False)
+                curr_pcl.paint_uniform_color([0.5,0.5,0.5])
                 viewer(pose, scene.pcl2open3d(stable=config['viewer']['stable']), add_pcd=curr_pcl, frame=slam.get_frame(), synth_frame=slam.get_rendered_frame())
             trajectory.append({'camera-pose': pose.tolist(), 'timestamp': img_number, 'residual': 0.0, 'key_frame': True})
             if len(trajectory) > nsamples:
