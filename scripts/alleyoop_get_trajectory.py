a998749e0dd7d364de4db467ba1d9b9c636536e7
mhayoz <mi.hayoz@sunrise.ch>
2022-05-20 15:12:17 +0200

    start, stop step option for dataset sampling

--- scripts/alleyoop_get_trajectory.py
+++ scripts/alleyoop_get_trajectory.py
@@ -12,13 +12,13 @@ import open3d
 import matplotlib.pyplot as plt
 from alley_oop.fusion.surfel_map import SurfelMap
 from alley_oop.utils.trajectory import save_trajectory
-from dataset.dataset_utils import get_data, StereoVideoDataset
+from dataset.dataset_utils import get_data, StereoVideoDataset, SequentialSubSampler
 from dataset.transforms import Compose
 import warnings
 from torch.utils.data import DataLoader
 
 
-def main(input_path, output_path, config, device_sel, nsamples):
+def main(input_path, output_path, config, device_sel, nsamples, start, step):
     device = torch.device('cpu')
     if device_sel == 'gpu':
         if torch.cuda.is_available():
@@ -29,7 +29,8 @@ def main(input_path, output_path, config, device_sel, nsamples):
     dataset, calib = get_data(input_path, config['img_size'])
     slam = SLAM(torch.tensor(calib['intrinsics']['left']), config['slam'], img_shape=config['img_size']).to(device)
     dataset.transform = Compose([dataset.transform, slam.pre_process])  # add pre-processing to data loading (CPU)
-    loader = DataLoader(dataset, num_workers=1, pin_memory=True)
+    sampler = SequentialSubSampler(dataset, start, nsamples, step)
+    loader = DataLoader(dataset, num_workers=1, pin_memory=True, sampler=sampler)
 
     if isinstance(dataset, StereoVideoDataset):
         disp_model = DisparityModel(calibration=calib, device=device, depth_clipping=config['depth_clipping'])
@@ -57,8 +58,6 @@ def main(input_path, output_path, config, device_sel, nsamples):
                 viewer(pose.cpu(), scene.pcl2open3d(stable=config['viewer']['stable']), add_pcd=curr_pcl,
                        frame=slam.get_frame(), synth_frame=slam.get_rendered_frame(), optim_results=slam.get_optimization_res())
             trajectory.append({'camera-pose': pose.tolist(), 'timestamp': img_number[0], 'residual': 0.0, 'key_frame': True})
-            if len(trajectory) > nsamples:
-                break
             if (i%50) == 0:
                 pcl = scene.pcl2open3d(stable=True)
                 open3d.io.write_point_cloud(os.path.join(output_path, f'map_{i:04d}.ply'), pcl)
@@ -110,7 +109,19 @@ if __name__ == '__main__':
         '--nsamples',
         type=int,
         default=10000000000,
-        help='force use of CPU.'
+        help='number of samples to run for.'
+    )
+    parser.add_argument(
+        '--start',
+        type=int,
+        default=0,
+        help='at which sample to start slam.'
+    )
+    parser.add_argument(
+        '--step',
+        type=int,
+        default=1,
+        help='sub sampling interval.'
     )
     args = parser.parse_args()
     with open(args.config, 'r') as ymlfile:
@@ -118,4 +129,4 @@ if __name__ == '__main__':
     if args.outpath is None:
         args.outpath = os.path.join(args.input, 'data','alleyoop')
 
-    main(args.input, args.outpath, config, args.device, args.nsamples)
+    main(args.input, args.outpath, config, args.device, args.nsamples, args.start, args.step)
