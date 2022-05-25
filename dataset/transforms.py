11e9fc296e025418a0a78659c3e0d346c4389cee
mhayoz <mi.hayoz@sunrise.ch>
2022-05-25 08:20:57 +0200

    generic arguments

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -2,32 +2,37 @@ import cv2
 
 
 class RGBDTransform(object):
-    def __call__(self, left, depth, mask=None):
-        return left, depth, mask
+    def __call__(self, left, depth, mask=None, right=None, disp=None):
+        return left, depth, mask, right, disp
 
 
 class ResizeRGBD(RGBDTransform):
-    def __init__(self, size, disparity=True):
+    def __init__(self, size):
         self.size = int(size[0]), int(size[1])
-        self.disparity = disparity
 
-    def __call__(self, img, disp_or_depth=None, mask=None):
+    def __call__(self, img, depth=None, mask=None, right=None, disp=None):
         # resize with cropping to conserve aspect ratio
         h, w = img.shape[:2]
         scale = max(self.size[0] / w, self.size[1] / h)
         crop = w * scale - self.size[0], h * scale - self.size[1]
         img = cv2.resize(img, (int(w * scale), int(h * scale)))
         img = img[int(crop[1] / 2):int(h * scale - crop[1] / 2), int(crop[0] / 2):int(w * scale - crop[0] / 2)]
-        if disp_or_depth is not None:
-            disp_or_depth = cv2.resize(disp_or_depth, (int(w * scale), int(h * scale)), cv2.INTER_NEAREST)
-            disp_or_depth = disp_or_depth[int(crop[1] / 2):int(h*scale-crop[1] / 2), int(crop[0] / 2):int(w*scale-crop[0] / 2)]
+        if right is not None:
+            right = cv2.resize(right, (int(w * scale), int(h * scale)))
+            right = right[int(crop[1] / 2):int(h*scale-crop[1] / 2), int(crop[0] / 2):int(w*scale-crop[0] / 2)]
+        if depth is not None:
+            depth = cv2.resize(depth, (int(w * scale), int(h * scale)), cv2.INTER_NEAREST)
+            depth = depth[int(crop[1] / 2):int(h*scale-crop[1] / 2), int(crop[0] / 2):int(w*scale-crop[0] / 2)]
+        if disp is not None:
+            disp = cv2.resize(disp, (int(w * scale), int(h * scale)), cv2.INTER_NEAREST)
+            disp = disp[int(crop[1] / 2):int(h * scale - crop[1] / 2),
+                    int(crop[0] / 2):int(w * scale - crop[0] / 2)]
             # we need to scale the disparity when resizing
-            if self.disparity:
-                disp_or_depth *= scale
+            disp *= scale
         if mask is not None:
             mask = cv2.resize(mask, (int(w * scale), int(h * scale)), cv2.INTER_NEAREST)
             mask = mask[int(crop[1] / 2):int(h * scale - crop[1] / 2), int(crop[0] / 2):int(w * scale - crop[0] / 2)]
-        return img, disp_or_depth, mask
+        return img, depth, mask, right, disp
 
 
 class Compose(object):
@@ -39,7 +44,6 @@ class Compose(object):
         for tr in self.transforms:
             args = tr(*args)
         return args
-
 
 
 class StereoTransform(object):
