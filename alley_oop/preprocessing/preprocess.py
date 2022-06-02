316d91559aacd1921c1bf7c29d7cc705ace833c3
mhayoz <mi.hayoz@sunrise.ch>
2022-06-02 09:07:55 +0200

    add illumination compensation

--- alley_oop/preprocessing/preprocess.py
+++ alley_oop/preprocessing/preprocess.py
@@ -3,7 +3,10 @@ from numpy import ndarray
 import numpy as np
 import cv2
 from alley_oop.metrics.projected_photo_metrics import disparity_photo_loss
-
+from alley_oop.geometry.normals import normals_from_regular_grid
+from alley_oop.utils.pytorch import batched_dot_product
+from alley_oop.utils.rgb2gray import rgb2gray_t
+from alley_oop.geometry.pinhole_transforms import create_img_coords_t, reverse_project
 
 class PreProcess(object):
     def __init__(self, scale, depth_min, intrinsics, dtype=torch.float32, mask_specularities:bool=True):
@@ -42,7 +45,18 @@ class PreProcess(object):
         else:
             # use generic depth based uncertainty model
             confidence = torch.exp(-.5 * depth ** 2 / .6 ** 2)
+        # compensate illumination and transform to gray-scale image
+        img_orig = img.clone()
+        img = self.compensate_illumination(rgb2gray_t(img, ax0=0), depth, self.intrinsics)
 
+        import matplotlib.pyplot as plt
+        plt.subplot(131)
+        plt.imshow(img_orig.permute(1,2,0).numpy())
+        plt.subplot(132)
+        plt.imshow(rgb2gray_t(img_orig).permute(1, 2, 0).numpy())
+        plt.subplot(133)
+        plt.imshow(img.permute(1, 2, 0).numpy())
+        plt.show()
         return img, depth, mask, confidence
 
     def specularity_mask(self, img, spec_thr=0.96):
@@ -50,3 +64,30 @@ class PreProcess(object):
             We can easily mask them by looking for maximum intensity values in all color channels """
         mask = img.sum(axis=-1) < (3*spec_thr)
         return mask
+
+    def compensate_illumination(self, img, depth, intrinsics):
+        """
+        use lambertian model and inverse-square law to estimate the albedo
+
+        p = depth^2 * I / cos(alpha)
+        where cos(alpha) = n_surface * n_illumination
+        """
+        v, u = torch.meshgrid(torch.arange(img.shape[1]), torch.arange(img.shape[2]))
+
+        normals_light_src = torch.stack(
+            (u - intrinsics[0, 2], v - intrinsics[1, 2], intrinsics[0, 0] * torch.ones(img.shape[-2:])))
+        normals_light_src /= torch.linalg.norm(normals_light_src, ord=2, dim=0, keepdims=True)
+
+        img_pts = create_img_coords_t(depth.shape[-2], depth.shape[-1])
+        pts = reverse_project(img_pts, intrinsics, dpth=depth).T
+        surface_normals = normals_from_regular_grid(pts.view((*depth.shape[-2:], 3)))
+        # pad normals
+        pad = torch.nn.ReplicationPad2d((0, 1, 0, 1))
+        surface_normals = pad(surface_normals.permute(2, 0, 1))
+
+        cos_alpha = batched_dot_product(normals_light_src.reshape(3, -1).T, surface_normals.reshape(3, -1).T).reshape(1,
+                                                                                                                      *img.shape[
+                                                                                                                       -2:])
+        cos_alpha = torch.clamp(cos_alpha, 0.3, 1)
+        albedo = torch.clamp(depth ** 2 * img / cos_alpha, 0.0, 1.0)
+        return albedo
