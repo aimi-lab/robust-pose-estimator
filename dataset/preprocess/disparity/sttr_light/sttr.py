1e61dedddac7c490dbf340a0c352cfc76ea1a8ef
mhayoz <mi.hayoz@sunrise.ch>
2022-06-08 09:29:22 +0200

    add moved files

---
+++ dataset/preprocess/disparity/sttr_light/sttr.py
@@ -0,0 +1,94 @@
+#  Authors: Zhaoshuo Li, Xingtong Liu, Francis X. Creighton, Russell H. Taylor, and Mathias Unberath
+#
+#  Copyright (c) 2020. Johns Hopkins University - All rights reserved.
+
+import torch.nn as nn
+
+from other_slam_methods.stereo_slam.disparity.sttr_light.feat_extractor_backbone import build_backbone
+from other_slam_methods.stereo_slam.disparity.sttr_light.feat_extractor_tokenizer import build_tokenizer
+from other_slam_methods.stereo_slam.disparity.sttr_light.pos_encoder import build_position_encoding
+from other_slam_methods.stereo_slam.disparity.sttr_light.regression_head import build_regression_head
+from other_slam_methods.stereo_slam.disparity.sttr_light.transformer import build_transformer
+from other_slam_methods.stereo_slam.disparity.sttr_light.utilities.misc import NestedTensor
+
+
+class STTR(nn.Module):
+    """
+    STTR: it consists of
+        - backbone: contracting path of feature descriptor
+        - tokenizer: expanding path of feature descriptor
+        - pos_encoder: generates relative sine pos encoding
+        - transformer: computes self and cross attention
+        - regression_head: regresses disparity and occlusion, including optimal transport
+    """
+
+    def __init__(self, args):
+        super(STTR, self).__init__()
+        layer_channel = [64, 128, 128]
+
+        self.backbone = build_backbone(args)
+        self.tokenizer = build_tokenizer(args, layer_channel)
+        self.pos_encoder = build_position_encoding(args)
+        self.transformer = build_transformer(args)
+        self.regression_head = build_regression_head(args)
+
+        self._disable_batchnorm_tracking()
+        self._reset_parameters()
+        self._relu_inplace()
+
+    def _reset_parameters(self):
+        """
+        xavier initialize all params
+        """
+        for n, m in self.named_modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, (nn.GroupNorm, nn.LayerNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.zeros_(m.bias)
+
+    def _disable_batchnorm_tracking(self):
+        """
+        disable Batchnorm tracking stats to reduce dependency on dataset (this acts as InstanceNorm with affine when batch size is 1)
+        """
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.track_running_stats = False
+
+    def _relu_inplace(self):
+        """
+        make all ReLU inplace
+        """
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d):
+                m.inplace = True
+
+    def forward(self, x: NestedTensor):
+        """
+        :param x: input data
+        :return:
+            a dictionary object with keys
+            - "disp_pred" [N,H,W]: predicted disparity
+            - "occ_pred" [N,H,W]: predicted occlusion mask
+            - "disp_pred_low_res" [N,H//s,W//s]: predicted low res (raw) disparity
+        """
+        bs, _, h, w = x.left.size()
+
+        # extract features
+        feat = self.backbone(x)  # concatenate left and right along the dim=0
+        tokens = self.tokenizer(feat)  # 2NxCxHxW
+        pos_enc = self.pos_encoder(x)  # NxCxHx2W-1
+
+        # separate left and right
+        feat_left = tokens[:bs]
+        feat_right = tokens[bs:]  # NxCxHxW
+
+        # transformer
+        attn_weight = self.transformer(feat_left, feat_right, pos_enc)
+
+        # regress disparity and occlusion
+        output = self.regression_head(attn_weight, x)
+
+        return output
---
+++ dataset/preprocess/segmentation_network/seg_model.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+from torchvision.transforms import Compose, Normalize
+from other_slam_methods.stereo_slam.segmentation_network.decoder import DeepLabV3PlusDecoder
+from other_slam_methods.stereo_slam.segmentation_network.pvt import pvt_v2_b2
+import segmentation_models_pytorch as smp
+from other_slam_methods.stereo_slam.segmentation_network.temporal_attention import DeepLabTAM
+import os
+from other_slam_methods.stereo_slam.segmentation_network.tmp_scaler import TempScaling
+
+
+def get_model(checkpoint):
+    assert os.path.isfile(checkpoint)
+    checkp = torch.load(checkpoint)
+    num_classes = checkp['config']['model']['num_classes']
+
+    model = smp.DeepLabV3Plus(
+        encoder_name='efficientnet-b0',  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+        encoder_weights="imagenet",  # use `imagenet` pre-trained weights for encoder initialization
+        encoder_output_stride=16,
+        classes=num_classes,
+    )
+
+    model.encoder = pvt_v2_b2()
+    model.decoder = DeepLabV3PlusDecoder(
+        encoder_channels=model.encoder.out_channels,
+        out_channels=model.decoder.out_channels,
+        atrous_rates=(12, 24, 36),
+        output_stride=16)
+    model = DeepLabTAM(model, input_size=checkp['config']['data'][0]['train']['img_size'])
+    model.load_state_dict(checkp['model_state'])
+    model = model.segmentation_model
+    return model, num_classes
+
+
+class ToTensor(object):
+    def __call__(self, input):
+        if torch.is_tensor(input):
+            return input.unsqueeze(0).permute(0,3,1,2).float()/255.0
+        else:
+            return torch.tensor(input).unsqueeze(0).permute(0,3,1,2).float() / 255.0
+
+
+class SemanticSegmentationModel(nn.Module):
+    def __init__(self, checkpoint, device, temperature=1.0, background_prior=1.0):
+        super(SemanticSegmentationModel, self).__init__()
+        self.model, self.n_classes = get_model(checkpoint)
+        self.model.to(device)
+        prior = torch.ones(self.n_classes)
+        prior[0] = background_prior
+        self.calibration = TempScaling(apply_softmax=True, temperature=temperature, prior=prior).to(device)
+        self.transform = Compose([ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+        self.device = device
+        self.model.eval()
+        self.eval()
+        self.tool_ids = torch.tensor([1, 2, 3, 8, 9, 11, 12], dtype=torch.uint8, device=device) # tool channel indicies
+
+    def get_mask(self, image):
+        with torch.no_grad():
+            is_numpy = not torch.is_tensor(image)
+            image = self.transform(image).to(self.device)
+            soft_predictions = self.forward(image)
+            mask = self.get_mask_from_prediction(soft_predictions)
+            soft_predictions = soft_predictions.squeeze(0)
+        if is_numpy:
+            mask = mask.cpu().numpy()
+            soft_predictions = soft_predictions.cpu().numpy()
+        return mask, soft_predictions
+
+    def get_mask_from_prediction(self, predictions):
+        predictions = torch.argmax(predictions, dim=-1)
+        mask = torch.ones(predictions.shape[:2], dtype=torch.uint8, device=self.device)
+        mask[(predictions[..., None] == self.tool_ids).any(-1)] = 0
+        return mask
+
+    def segment(self, image):
+        with torch.no_grad():
+            image = self.transform(image)
+            predictions = self.forward(image)
+            return predictions
+
+    def forward(self, img):
+        out = self.model(img)
+        out = self.calibration(out)
+        out = out.squeeze(0).permute(1,2,0)
+        return out
+
+
