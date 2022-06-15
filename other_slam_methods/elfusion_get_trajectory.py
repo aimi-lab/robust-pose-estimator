2ca3cf8bd7edcbd53ad2e604a181989a1a5796f5
mhayoz <mi.hayoz@sunrise.ch>
2022-06-15 08:32:25 +0200

    fix missing import

--- other_slam_methods/elfusion_get_trajectory.py
+++ other_slam_methods/elfusion_get_trajectory.py
@@ -14,6 +14,7 @@ import warnings
 import wandb
 from torch.utils.data import DataLoader
 from scipy.spatial.transform import Rotation as R
+from scripts.evaluate_ate_freiburg import main as evaluate
 
 
 def save_ply(pcl_array, path):
