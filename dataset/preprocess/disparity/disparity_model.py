6a969de388c220d81e9d66459dbd9105dd0bf680
mhayoz <mi.hayoz@sunrise.ch>
2022-06-30 09:31:37 +0200

    add normalization

--- dataset/preprocess/disparity/disparity_model.py
+++ dataset/preprocess/disparity/disparity_model.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 import yaml
 import os
 from dataset.preprocess.disparity.psmnet.stacked_hourglass import PSMNet
-from torchvision.transforms import ToTensor, Resize
+from torchvision.transforms import ToTensor, Resize, Normalize
 import warnings
 
+IMAGENET_STATS = {'mean': [0.485, 0.456, 0.406],
+                   'std': [0.229, 0.224, 0.225]}
 
 class Struct:
     def __init__(self, **entries):
@@ -30,27 +32,15 @@ class DisparityModel(nn.Module):
         self.upscale_factor = calibration['img_size'][0]/self.tw
         self.resize_for_inf = Resize((self.th, self.tw))
         self.resize_to_orig = Resize((calibration['img_size'][1], calibration['img_size'][0]))
+        self.normalize = Normalize(**IMAGENET_STATS)
 
     def forward(self, limg, rimg):
-        is_numpy = False
-        if not torch.is_tensor(limg):
-            limg = self.tensor(limg).to(self.device)
-            rimg = self.tensor(rimg).to(self.device)
-            is_numpy = True
-        if limg.ndim == 3:
-            limg = limg.unsqueeze(0)
-            rimg = rimg.unsqueeze(0)
         with torch.no_grad():
-            limg = self.resize_for_inf(limg)
-            rimg = self.resize_for_inf(rimg)
-
+            limg = self.normalize(self.resize_for_inf(limg))
+            rimg = self.normalize(self.resize_for_inf(rimg))
             disp = self.model(limg, rimg)
             disp = self.resize_to_orig(disp)*self.upscale_factor
-            clip_mask = torch.ones_like(disp, dtype=bool)
             depth = self.baseline_f / disp
-        if is_numpy:
-            disp = disp.cpu().numpy()
-            depth = depth.cpu().numpy()
         return disp, depth
 
     def _load(self, config):
