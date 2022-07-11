94d8904dbb49955ece148647855119997400928e
mhayoz <mi.hayoz@sunrise.ch>
2022-07-11 08:33:04 +0200

    add efusion

--- other_slam_methods/benchmark_scared.py
+++ other_slam_methods/benchmark_scared.py
@@ -4,6 +4,7 @@ from other_slam_methods.orbslam2_get_trajectory import main as orbslam
 import yaml
 from multiprocessing import Process
 import multiprocessing as mp
+import shutil
 
 PATH_REPLACEMENT = ['/home/mhayoz/research', '/storage/workspaces/artorg_aimi/ws_00000']
 if __name__ == '__main__':
@@ -12,20 +13,20 @@ if __name__ == '__main__':
     mp.set_start_method('spawn')
     for sequence in sequences:
         print(sequence)
-
+        sequence = sequence.replace(PATH_REPLACEMENT[1], PATH_REPLACEMENT[0])
+        sequence = sequence.replace('\n', '')
         try:
-            sequence = sequence.replace(PATH_REPLACEMENT[1], PATH_REPLACEMENT[0])
-            sequence = sequence.replace('\n', '')
-            with open('configuration/efusion_scared.yaml', 'r') as ymlfile:
-                config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
-            if os.path.isfile(os.path.join(sequence, 'data/efusion/trajectory.json')):
-                continue
-            #elfusion(sequence, os.path.join(sequence, '/data/efusion'), config, 'cpu', 0, 10000000000, 1, None)#, 'efusion')
-
-            with open('configuration/orbslam2.yaml', 'r') as ymlfile:
-                config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
-            p = Process(target=orbslam, args=(sequence, os.path.join(sequence, 'data/efusion'), config, 'cpu', 0, 1000000000, 1, 'orbslam2', False,))
-            p.start()
-            p.join()
+            if not os.path.isfile(os.path.join(sequence, 'data/efusion/trajectory.json')):
+                with open('configuration/efusion_scared.yaml', 'r') as ymlfile:
+                    config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+                p = Process(target=elfusion, args=(sequence, os.path.join(sequence, 'data/efusion'), config, 'cpu', 0, 10000000, 1, 'efusion', False))
+                p.start()
+                p.join()
+            if not os.path.isfile(os.path.join(sequence, 'data/orbslam2/trajectory.json')):
+                with open('configuration/orbslam2.yaml', 'r') as ymlfile:
+                    config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+                p = Process(target=orbslam, args=(sequence, os.path.join(sequence, 'data/orbslam2'), config, 'cpu', 0, 1000000000, 1, 'orbslam2', False,))
+                p.start()
+                p.join()
         except:
             print('not successfull. -> skip')
