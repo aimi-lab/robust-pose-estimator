33fefc778dccf8b4252512848ac3528988651e32
mhayoz <mi.hayoz@sunrise.ch>
2022-07-14 09:45:43 +0200

    use freiburg files

--- scripts/trajectory_plots.py
+++ scripts/trajectory_plots.py
@@ -1,55 +1,11 @@
-from pathlib import Path
-from typing import List
+import os
 import numpy as np
-import json
 import argparse
 import matplotlib as mpl
 mpl.use('Qt5Agg')
-from alley_oop.utils.paths import get_scared_abspath
 from alley_oop.pose.trajectory_analyzer import TrajectoryAnalyzer
-
-
-def load_scared_pose(fnames:List=None) -> np.ndarray:
-
-    # if kinematics data collect all files
-    if str(fnames[0]).lower().__contains__('frame_data') or str(fnames[0]).lower().__contains__('superglue'):
-        pose_list = []
-        for fname in fnames:
-            with open(str(fname), 'r') as f: pose_elem = json.load(f)
-            pose = np.array(pose_elem['camera-pose'])
-            pose_list.append(pose)
-
-    elif str(fnames[0]).lower().__contains__('orbslam'): # ORBSLAM2
-        fname = fnames[0]
-        with open(str(fname), 'r') as f: pose_elem_list = json.load(f)
-        pose_list = []
-        for pose_elem in pose_elem_list:
-            pose = np.array(pose_elem['camera-pose'])
-            pose[0:3, 3] = -pose[0:3, 3]  # neg. translation as Intuitive's coordinate system is inverted wrt. OpenCV
-            pose[0:3, 0:3] = pose[0:3, 0:3].T  # invert rotation as Intuitive's coordinate system is inverted wrt. OpenCV
-            pose_list.append(pose)
-    elif str(fnames[0]).lower().__contains__('efusion') | str(fnames[0]).lower().__contains__('combined')| str(fnames[0]).lower().__contains__('alleyoop') :  # ORBSLAM2:
-        fname = fnames[0]
-        with open(str(fname), 'r') as f: pose_elem_list = json.load(f)
-        pose_list = []
-        for pose_elem in pose_elem_list:
-            pose = np.array(pose_elem['camera-pose'])
-            pose[0:3, 3] = -pose[0:3, 3]  # neg. translation as Intuitive's coordinate system is inverted wrt. OpenCV
-            pose[0:3, 0:3] = pose[0:3, 0:3].T
-            #pose[:3,3] *= 1000/15
-            pose_list.append(pose)
-    else:
-        fname = fnames[0]
-        with open(str(fname), 'r') as f:
-            pose_elem_list = json.load(f)
-        pose_list = []
-        for pose_elem in pose_elem_list:
-            pose = np.array(pose_elem['camera-pose'])
-            pose_list.append(pose)
-    
-    pose_arrs = np.array(pose_list)[:, :4, :4]
-
-    return pose_arrs
+from alley_oop.utils.trajectory import read_freiburg
+from scripts.evaluate_ate_freiburg import main as evaluate
 
 
 if __name__ == '__main__':
@@ -65,7 +21,7 @@ if __name__ == '__main__':
         '--pred_folders',
         type=str,
         nargs='+',
-        default='orbslam2_rgbd_results',
+        default='gt',
         help='Folder containing predictions.'
     )
     args = parser.parse_args()
@@ -73,19 +29,22 @@ if __name__ == '__main__':
     colors = ['b', 'g', 'r', 'k', 'm', 'y']
     d_idx = 1
 
-    # iterate through keyframes
-    for k_idx in range(1, 4):
-        pose_plotter = TrajectoryAnalyzer(title='dataset_'+str(d_idx)+', keyframe_'+str(k_idx))
-        for k, meth in enumerate(args.pred_folders):
-            print(meth)
-            fnames = sorted((Path(args.base_path) / 'data' / meth).rglob('*.json'))
-            pose_arrs = load_scared_pose(fnames)
-            pose_arrs[:, :3, -1] = np.cumsum(pose_arrs[:, :3, -1], axis=0)/-5 if meth.lower().__contains__('defslam') else pose_arrs[:, :3, -1]
-            pose_arrs[:, :3, -1] = np.cumsum(pose_arrs[:, :3, -1]*np.array([-1, 1, -1]), axis=0) if meth.lower().__contains__('superglue') else pose_arrs[:, :3, -1]
-            pose_arrs[:, :2, -1] = pose_arrs[:, :2, -1][:, ::-1] if meth.lower().__contains__('superglue') else pose_arrs[:, :2, -1]
-            color = colors[k%len(colors)]
-            pose_plotter.add_pose_trajectory(pose_arrs, label=meth, color=color)
-        pose_plotter.legend()
-        pose_plotter.get_rmse_by_idx(idx_a=-1, idx_b=-2, plot_opt=True) if len(args.base_path) > 1 else None
-        pose_plotter.write_file()
-        pose_plotter.show()
+    keyframe = os.path.basename(args.base_path)
+    dataset = os.path.basename(os.path.dirname(args.base_path))
+    pose_plotter = TrajectoryAnalyzer(title=dataset + '/' + keyframe)
+
+    freiburg_paths = {m: os.path.join(args.base_path, 'data', m, 'trajectory.freiburg') for m in args.pred_folders}
+    freiburg_paths.update({'gt': os.path.join(args.base_path, 'groundtruth.txt')})
+
+    for k, meth in enumerate(freiburg_paths):
+        print(meth)
+        assert os.path.isfile(freiburg_paths[meth]), f'{meth} does not exist'
+        error = evaluate(freiburg_paths[meth], freiburg_paths['gt'])*1000
+        print('ATE-RMSE: ',np.sqrt(np.dot(error, error) / len(error)), ' mm')
+        pose_arrs = np.stack(read_freiburg(freiburg_paths[meth]))
+        color = colors[k%len(colors)]
+        pose_plotter.add_pose_trajectory(pose_arrs, label=meth, color=color)
+    pose_plotter.legend()
+    #pose_plotter.get_rmse_by_idx(idx_a=-1, idx_b=-2, plot_opt=True) if len(args.base_path) > 1 else None
+    pose_plotter.write_file()
+    pose_plotter.show()
