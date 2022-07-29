2e25241ffe616e2011da567700878961bdef4d38
mhayoz <mi.hayoz@sunrise.ch>
2022-07-29 16:18:55 +0200

    fix pose aggregation

--- alley_oop/pose/attention_pose_estimator.py
+++ alley_oop/pose/attention_pose_estimator.py
@@ -20,18 +20,21 @@ class AttentionPoseEstimator(torch.nn.Module):
         self.frame2frame = config['frame2frame']
 
     def estimate(self, frame: FrameClass, scene: SurfelMap):
-        ret_frame = None
         if self.frame2frame:
             if self.last_frame is not None:
-                self.last_pose.data = self.model(self.last_frame.img, frame.img)
+                rel_pose= self.model(self.last_frame.img, frame.img)
                 ret_frame = self.last_frame
+            else:
+                rel_pose = self.last_pose.data.clone()
+                ret_frame = None
             self.last_frame = frame
         else:
             # transform scene to last camera pose coordinates
             scene_tlast = scene.transform_cpy(inv_transform(self.last_pose))
             model_frame = scene_tlast.render(self.intrinsics)
-            self.last_pose.data = self.model(model_frame.img, frame.img)
+            rel_pose = self.model(model_frame.img, frame.img)
             ret_frame = model_frame
+        self.last_pose.data = self.last_pose.data @ torch.linalg.inv(rel_pose)
         return self.last_pose, ret_frame
 
     @property
