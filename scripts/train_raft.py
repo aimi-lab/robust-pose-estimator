2c753043023b2126da21edf3dc4aa924526107a6
mhayoz <mi.hayoz@sunrise.ch>
2022-08-10 15:57:23 +0200

    fix num workers

--- scripts/train_raft.py
+++ scripts/train_raft.py
@@ -96,7 +96,7 @@ def main(args, config, force_cpu):
                                              config['image_shape'], step=config['data']['val']['step'])
     print(f"train: {len(data_train)} samples, val: {len(data_val)} samples")
     intrinsics = intrinsics.to(device)
-    train_loader = DataLoader(data_train, num_workers=0, pin_memory=True, batch_size=config['train']['batch_size'], shuffle=True)
+    train_loader = DataLoader(data_train, num_workers=4, pin_memory=True, batch_size=config['train']['batch_size'], shuffle=True)
     val_loader = DataLoader(data_val, num_workers=4, pin_memory=True, batch_size=config['val']['batch_size'], sampler=SubsetRandomSampler(torch.from_numpy(np.random.choice(len(data_val), size=(400,), replace=False))))
     optimizer, scheduler = fetch_optimizer(config['train'], model)
 
