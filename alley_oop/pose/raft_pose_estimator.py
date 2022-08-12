0b147d456cbe4fdf9369aa7bf49f411c34fe97ff
mhayoz <mi.hayoz@sunrise.ch>
2022-08-12 13:47:26 +0200

    remove depreciated resize

--- alley_oop/pose/raft_pose_estimator.py
+++ alley_oop/pose/raft_pose_estimator.py
@@ -32,14 +32,10 @@ class RAFTPoseEstimator(torch.nn.Module):
         self.resize = Resize((checkp['config']['image_shape'][0]//8,checkp['config']['image_shape'][1]//8))
 
     def estimate(self, frame: FrameClass, scene: SurfelMap):
-        depth = self.resize(frame.depth)
-        confidence = self.resize(frame.confidence)
         if self.frame2frame:
             if self.last_frame is not None:
-                last_depth = self.resize(self.last_frame.depth)
-                last_conf = self.resize(self.last_frame.confidence)
-                rel_pose_se3 = self.model(self.last_frame.img, frame.img, last_depth, depth, last_conf, confidence)[1][-1].squeeze(0)
-                rel_pose = lie_se3_to_SE3(rel_pose_se3/1000.0)
+                rel_pose_se3 = self.model(self.last_frame.img, frame.img, self.last_frame.depth, frame.depth, self.last_frame.confidence, frame.confidence)[1][-1].squeeze(0)
+                rel_pose = lie_se3_to_SE3(rel_pose_se3)
                 ret_frame = self.last_frame
             else:
                 rel_pose = self.last_pose.data.clone()
@@ -49,10 +45,8 @@ class RAFTPoseEstimator(torch.nn.Module):
             # transform scene to last camera pose coordinates
             scene_tlast = scene.transform_cpy(inv_transform(self.last_pose))
             model_frame = scene_tlast.render(self.intrinsics)
-            model_depth = self.resize(model_frame.depth)
-            model_conf = self.resize(model_frame.confidence)
-            rel_pose_se3 = self.model(model_frame.img, frame.img, model_depth, depth, model_conf, confidence)[1][-1].squeeze(0)
-            rel_pose = lie_se3_to_SE3(rel_pose_se3/1000.0)
+            rel_pose_se3 = self.model(model_frame.img, frame.img, model_frame.depth, frame.depth, model_frame.confidence, frame.confidence)[1][-1].squeeze(0)
+            rel_pose = lie_se3_to_SE3(rel_pose_se3)
             ret_frame = model_frame
         self.last_pose.data = self.last_pose.data @ torch.linalg.inv(rel_pose)
         return self.last_pose, ret_frame
