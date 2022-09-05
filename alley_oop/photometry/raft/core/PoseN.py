d7180d34934a9bf39408f706087d7bf50372702a
mhayoz <mi.hayoz@sunrise.ch>
2022-09-05 09:25:20 +0200

    add horn pose estimation

--- alley_oop/photometry/raft/core/PoseN.py
+++ alley_oop/photometry/raft/core/PoseN.py
@@ -9,6 +9,7 @@ from alley_oop.geometry.pinhole_transforms import create_img_coords_t
 from alley_oop.photometry.raft.core.corr import CorrBlock, AlternateCorrBlock
 from alley_oop.photometry.raft.core.raft import RAFT
 from alley_oop.photometry.raft.core.extractor import RGBDEncoder
+from alley_oop.geometry.absolute_pose_quarternion import align_torch
 
 
 class PoseHead(nn.Module):
@@ -20,10 +21,11 @@ class PoseHead(nn.Module):
         self.mlp = nn.Sequential(nn.Linear(in_features=input_dims+6,out_features=64),
                                     nn.ReLU(),
                                     nn.Linear(in_features=64, out_features=6))
+
     def forward(self, net, flow, pcl1, pcl2, pose):
-        pcl2 = self.remap(pcl2, flow)
-        out = self.convs(torch.cat((net, flow, pcl1, pcl2), dim=1)).view(net.shape[0], -1)
-        return self.mlp(torch.cat((out, pose), dim=1))
+        pcl_aligned = self.remap(pcl2, flow)
+        out = self.convs(torch.cat((net, flow, pcl1, pcl_aligned), dim=1)).view(net.shape[0], -1)
+        return self.mlp(torch.cat((out, pose), dim=1)), align_torch(pcl1.view(*pcl1.shape[:2], -1), pcl_aligned.view(*pcl1.shape[:2], -1))
 
     def remap(self, x, flow):
         # get optical flow correspondences
@@ -103,6 +105,7 @@ class PoseN(RAFT):
 
         flow_predictions = []
         pose_se3_predictions = []
+        pose_se3_direct = []
         for itr in range(iters):
             coords1 = coords1.detach()
             corr = corr_fn(coords1)  # index correlation volume
@@ -110,8 +113,8 @@ class PoseN(RAFT):
             flow = coords1 - coords0
             with torch.cuda.amp.autocast():
                 net, up_mask, delta_flow = self.update_block(net, inp, corr, flow)
-            delta_pose = self.pose_head(net, flow, pcl1, pcl2, pose_se3)
-
+            delta_pose, pose_direct = self.pose_head(net, flow, pcl1, pcl2, pose_se3)
+            pose_se3_direct.append(pose_direct)
             # F(t+1) = F(t) + \Delta(t)
             coords1 = coords1 + delta_flow
             flow_up = coords1 - coords0
@@ -119,7 +122,7 @@ class PoseN(RAFT):
 
             pose_se3 = pose_se3 + delta_pose
             pose_se3_predictions.append(pose_se3.float()/self.pose_scale)
-        return flow_predictions, pose_se3_predictions
+        return flow_predictions, pose_se3_predictions, pose_se3_direct
 
     def init_from_raft(self, raft_ckp):
         raft = RAFT(self.config)
