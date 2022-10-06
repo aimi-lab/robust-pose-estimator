a0a10f24e12c0d7283202d400902c677b9cf2b63
mhayoz <mi.hayoz@sunrise.ch>
2022-10-06 08:39:27 +0200

    fix gt extraction

--- evaluation/extract_gt_slam.py
+++ evaluation/extract_gt_slam.py
@@ -15,11 +15,20 @@ if __name__ == '__main__':
         print(sequence)
         sequence = sequence.replace(PATH_REPLACEMENT[1], PATH_REPLACEMENT[0])
         sequence = sequence.replace('\n', '')
-        if not os.path.isfile(os.path.join(sequence, 'groundtruth.txt')):
-            file = glob.glob(os.path.join(sequence, "*.json"))
-            assert len(file) == 1
-            # read poses
-            poses = read_json_intuitive(file[0], with_stamp=False)
-            poses = [{"camera-pose": p, "timestamp":i} for i,p in enumerate(poses)]
-            save_freiburg(poses, sequence)
-            shutil.move(os.path.join(sequence, 'trajectory.freiburg'), os.path.join(sequence, 'groundtruth.txt'))
+        file = glob.glob(os.path.join(sequence, "*.json"))
+        assert len(file) == 1
+        # read poses
+        poses = read_json_intuitive(file[0], with_stamp=False)
+        # seems to be in meters -> transform to mm
+        poses = np.stack(poses)
+        poses[:, :3,3] *= 1000
+        first_pose = np.linalg.inv(poses[0])
+        poses = first_pose[None,...] @ poses
+
+        isi_t = np.eye(4)
+        isi_t[0,0] = -1
+        isi_t[1,1] = -1
+        poses = isi_t[None,...] @ poses @ isi_t[None,...]
+        poses = [{"camera-pose": p, "timestamp":i} for i,p in enumerate(poses)]
+        save_freiburg(poses, sequence)
+        shutil.move(os.path.join(sequence, 'trajectory.freiburg'), os.path.join(sequence, 'groundtruth.txt'))
