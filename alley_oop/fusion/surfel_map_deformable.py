0ec4e581582c20ed52d9e1006ae5255d2b319f83
mhayoz <mi.hayoz@sunrise.ch>
2022-10-12 09:52:56 +0200

    fix actual warp bug and back to optical flow

--- alley_oop/fusion/surfel_map_deformable.py
+++ alley_oop/fusion/surfel_map_deformable.py
@@ -1,19 +1,15 @@
-from alley_oop.fusion.surfel_map import *
-from alley_oop.interpol.gp_warpfield import GP_WarpFieldEstimator
+from alley_oop.fusion.surfel_map_flow import *
 from alley_oop.utils.pytorch import MedianPool2d
 from alley_oop.interpol.sparse_img_interpolation import SparseMedianInterpolator
 
 
-class SurfelMapDeformable(SurfelMap):
+class SurfelMapDeformable(SurfelMapFlow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.warp_field = kwargs["warp_field"] if "warp_field" in kwargs else torch.zeros_like(self.opts)  # translational warp-field
         assert self.opts.shape == self.warp_field.shape
-        self.warp_field_estimator = GP_WarpFieldEstimator(length_scale=0.1,
-                                                          noise_level=0.001)
         self.median_filt = MedianPool2d(15, same=True, stride=1)
         self.interpolate = SparseMedianInterpolator(5)
-        #self.dbug_opt = True
 
     def fuse(self, *args):
         frame, pmat, flow, render_csp = args
@@ -38,26 +34,29 @@ class SurfelMapDeformable(SurfelMap):
         # project surfel map to current image plane
         opts_def = self._deform(self.opts, self.warp_field)
         global_ipts = forward_project(opts_def, kmat=kmat, rmat=pmat_inv[:3, :3], tvec=pmat_inv[:3, -1][:, None])
+
+        # consider masked surfels and enforce channel x samples shape
         rgb = rgb.view(3, -1)
 
         # use optical flow to get correspondences (2D to 3D flow)
-        proj_ref_idx = (global_ipts[0, :] >= 0) & (global_ipts[1, :] >= 0) & \
+        flow_trg_idx, flow_ref_idx, valid = self.get_flow_correspondences(frame, flow, render_csp)
+        bidx = (global_ipts[0, :] >= 0) & (global_ipts[1, :] >= 0) & \
                (global_ipts[0, :] < self.img_shape[1]-1) & (global_ipts[1, :] < self.img_shape[0]-1)
+        proj_trg_idx = self.get_match_indices(global_ipts[:, bidx])
 
-        proj_trg_idx = self.get_match_indices(global_ipts[:, proj_ref_idx])
-
-        # fit and predict warp-field for canonical model to current frame
-        proj_trg_idx, proj_ref_idx = self.estimate_warpfield(opts, proj_ref_idx, proj_trg_idx)
+        self.estimate_warpfield(opts, flow_ref_idx, flow_trg_idx, valid, global_ipts, bidx)
         # pre-select confidence elements
         conf = torch.ones_like(frame.confidence.view(1, -1)) / self.conf_thr
 
         # update confidences
-        self.conf[:, proj_ref_idx] = torch.clamp(self.conf[:, proj_ref_idx] + conf[:, proj_trg_idx], 0.0, 1.0)  # saturate confidence to 1
+        self.conf[:, flow_ref_idx[valid]] = torch.clamp(self.conf[:, flow_ref_idx[valid]] + conf[:, flow_trg_idx[valid]], 0.0, 1.0)  # saturate confidence to 1
 
         #self.remove_redundant_surfels(bidx, render_csp)
 
         # create mask identifying unmatched indices
         mask = torch.ones(opts.shape[1], device=opts.device, dtype=torch.bool)
+
+        mask[flow_trg_idx[valid]] = False  # mask points with optical flow matches
         mask[proj_trg_idx] = False  # mask points with projective associations
         # avoid fusion with invalid pixels
         mask &= frame.mask.view(-1)
@@ -120,7 +119,6 @@ class SurfelMapDeformable(SurfelMap):
         map = self._constructor(opts=opts, kmat=self.kmat, rgb=self.rgb, img_shape=self.img_shape,
                                   depth_scale=self.depth_scale, conf=self.conf, warp_field=warp_field,
                                   normals=self.normals).to(self.device)
-
         return map
 
     def transform(self, transform:torch.tensor):
@@ -141,21 +139,24 @@ class SurfelMapDeformable(SurfelMap):
         else:
             return super().render(intrinsics, extrinsics)
 
-    def estimate_warpfield(self, opts, ref_idx, trg_idx):
+    def estimate_warpfield(self, opts, flow_ref_idx, flow_trg_idx, valid, ipts, bidx):
         # use the residuals as a 3d translational warp-field
         # the assumptions are:
         # 1: small residuals are due to depth, flow or pose estimation errors and should be ignored
         # 2: large residuals are mainly due to deformations
         # -> no accumulation of small drift errors, but update of scene when deformations are significant
-        self.warp_field = torch.zeros_like(self.opts)
-        self.warp_field[:, ref_idx] = opts[:, trg_idx] - self.opts[:, ref_idx]
-        diff = torch.sum(self.warp_field ** 2, dim=0)
-        not_deformed = diff <= self.d_thresh ** 2
+        warp = opts[:, flow_trg_idx].view(3,*self.img_shape) - self.opts[:, flow_ref_idx].view(3,*self.img_shape)
+        warp[:, ~valid.view(self.img_shape)] = 0.0
+        warp = self.median_filt(warp.unsqueeze(0)).squeeze()
 
-        valid = diff[ref_idx] <= (10*self.d_thresh) ** 2
+        self.warp_field = torch.zeros_like(self.opts)
+        # use projective association to interpolate warp-field for not rendered points
+        self.warp_field[0, bidx] = warp[0][(ipts[1, bidx].long(), ipts[0, bidx].long())]
+        self.warp_field[1, bidx] = warp[1][(ipts[1, bidx].long(), ipts[0, bidx].long())]
+        self.warp_field[2, bidx] = warp[2][(ipts[1, bidx].long(), ipts[0, bidx].long())]
+        not_deformed = torch.sum(self.warp_field ** 2, dim=0) <= self.d_thresh ** 2
         self.warp_field[:, not_deformed] = 0.0
-        ref_idx[ref_idx.clone()] &= valid
-        return trg_idx[valid], ref_idx
+        return self.warp_field
 
     def remove_redundant_surfels(self, in_fov, rendered):
         # remove points that are in the FOV but not rendered
@@ -180,11 +181,6 @@ class SurfelMapDeformable(SurfelMap):
 
     def remove_surfels_by_confidence_and_time(self):
         """ remove unstable points that have been created long time ago """
-
-        ok_pts = ((self.conf >= 1.0) | ((self.tick - self.t_created) < self.t_max)).squeeze()
-        self.opts = self.opts[:, ok_pts]
-        self.rgb = self.rgb[:, ok_pts]
-        self.conf = self.conf[:, ok_pts]
-        self.t_created = self.t_created[:, ok_pts]
+        ok_pts = super().remove_surfels_by_confidence_and_time()
         self.warp_field = self.warp_field[:, ok_pts]
         return ok_pts
