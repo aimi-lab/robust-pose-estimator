2fc52ea90e76ea9571c56e32c072b969b7de2797
mhayoz <mi.hayoz@sunrise.ch>
2022-10-14 09:47:19 +0200

    add 2d viewer

--- scripts/alleyoop_get_trajectory.py
+++ scripts/alleyoop_get_trajectory.py
@@ -2,43 +2,39 @@ import sys
 sys.path.append('../')
 from alley_oop.slam import SLAM
 import os
-import torch
-import numpy as np
 from tqdm import tqdm
 from dataset.preprocess.segmentation_network.seg_model import SemanticSegmentationModel
-import matplotlib.pyplot as plt
 from alley_oop.fusion.surfel_map_deformable import *
 from alley_oop.utils.trajectory import save_trajectory, read_freiburg
 from dataset.dataset_utils import get_data, StereoVideoDataset, SequentialSubSampler, RGBDDataset, TUMDataset
-from dataset.transforms import Compose
 import warnings
 from torch.utils.data import DataLoader
 import wandb
 from evaluation.evaluate_ate_freiburg import main as evaluate
 
 
-def main(input_path, output_path, config, device_sel, stop, start, step, log, force_video, checkpoint, store_maps):
+def main(args, config):
     device = torch.device('cpu')
-    if device_sel == 'gpu':
+    if args.device == 'gpu':
         if torch.cuda.is_available():
             device = torch.device('cuda')
         else:
             warnings.warn('No GPU available, fallback to CPU')
 
-    if log is not None:
-        config.update({'keyframe': os.path.split(input_path)[-1]})
-        config.update({'dataset': os.path.split(input_path)[-2]})
-        wandb.init(project='Alley-OOP', config=config, group=log)
+    if args.log is not None:
+        config.update({'keyframe': os.path.split(args.input)[-1]})
+        config.update({'dataset': os.path.split(args.input)[-2]})
+        wandb.init(project='Alley-OOP', config=config, group=args.log)
 
-    dataset, calib = get_data(input_path, config['img_size'], force_video=force_video, rect_mode=config['rect_mode'])
+    dataset, calib = get_data(args.input, config['img_size'], force_video=args.force_video, rect_mode=config['rect_mode'])
     # check for ground-truth pose data for logging purposes
-    gt_file = os.path.join(input_path, 'groundtruth.txt')
+    gt_file = os.path.join(args.input, 'groundtruth.txt')
     gt_trajectory = read_freiburg(gt_file) if os.path.isfile(gt_file) else None
 
     slam = SLAM(torch.tensor(calib['intrinsics']['left']).to(device), config['slam'], img_shape=config['img_size'], baseline=calib['bf'],
-                checkpoint=checkpoint, init_pose=torch.tensor(gt_trajectory[0]) if gt_trajectory is not None else torch.eye(4)).to(device)
+                checkpoint=args.checkpoint, init_pose=torch.tensor(gt_trajectory[0]) if gt_trajectory is not None else torch.eye(4)).to(device)
     if not isinstance(dataset, StereoVideoDataset):
-        sampler = SequentialSubSampler(dataset, start, stop, step)
+        sampler = SequentialSubSampler(dataset, args.start, args.stop, args.step)
     else:
         warnings.warn('start/stop arguments not supported for video dataset. ignored.', UserWarning)
         sampler = None
@@ -51,14 +47,16 @@ def main(input_path, output_path, config, device_sel, stop, start, step, log, fo
     slam.recorder.set_gt(gt_trajectory)
     with torch.no_grad():
         viewer = None
-        if config['viewer']['enable']:
+        if args.viewer == '3d':
             from viewer.viewer3d import Viewer3D
-            viewer = Viewer3D((2 * config['img_size'][0], 2 * config['img_size'][1]),
-                              blocking=config['viewer']['blocking'])
+            viewer = Viewer3D((2 * config['img_size'][0], 2 * config['img_size'][1]),blocking=args.block_viewer)
+        elif args.viewer == '2d':
+            from viewer.viewer2d import Viewer2D
+            viewer = Viewer2D(outpath=args.outpath, blocking=args.block_viewer)
 
         trajectory = []
-        os.makedirs(output_path, exist_ok=True)
-        for i, data in enumerate(tqdm(loader, total=min(len(dataset), (stop-start)//step))):
+        os.makedirs(args.outpath, exist_ok=True)
+        for i, data in enumerate(tqdm(loader, total=min(len(dataset), (args.stop-args.start)//args.step))):
             if isinstance(dataset, StereoVideoDataset):
                 limg, rimg, pose_kinematics, img_number = data
                 mask, semantics = seg_model.get_mask(limg.to(device))
@@ -83,23 +81,23 @@ def main(input_path, output_path, config, device_sel, stop, start, step, log, fo
                        def_pcd=deformed_scene)
 
             trajectory.append({'camera-pose': pose.tolist(), 'timestamp': img_number[0], 'residual': 0.0, 'key_frame': True})
-            if (log is not None) & (i > 0):
-                slam.recorder.log(step=i*step)
-            if store_maps & ((i%50) == 49):
-                scene.save_ply(os.path.join(output_path, f'stable_map_{i}.ply'), stable=True)
-                scene.deform_cpy().save_ply(os.path.join(output_path, f'def_map_{i}.ply'), stable=True)
+            if (args.log is not None) & (i > 0):
+                slam.recorder.log(step=i*args.step)
+            if args.store_map & ((i%50) == 49):
+                scene.save_ply(os.path.join(args.outpath, f'stable_map_{i}.ply'), stable=True)
+                scene.deform_cpy().save_ply(os.path.join(args.outpath, f'def_map_{i}.ply'), stable=True)
 
-        save_trajectory(trajectory, output_path)
+        save_trajectory(trajectory, args.outpath)
 
-        scene.save_ply(os.path.join(output_path, 'stable_map.ply'), stable=True)
-        scene.save_ply(os.path.join(output_path, 'all_map.ply'), stable=False)
-        if log is not None:
-            wandb.save(os.path.join(output_path, 'trajectory.freiburg'))
-            wandb.save(os.path.join(output_path, 'trajectory.json'))
-            wandb.save(os.path.join(output_path, 'map.ply'))
-            if os.path.isfile(os.path.join(input_path, 'groundtruth.txt')):
-                error = evaluate(os.path.join(input_path, 'groundtruth.txt'),
-                                 os.path.join(output_path, 'trajectory.freiburg'))
+        scene.save_ply(os.path.join(args.outpath, 'stable_map.ply'), stable=True)
+        scene.save_ply(os.path.join(args.outpath, 'all_map.ply'), stable=False)
+        if args.log is not None:
+            wandb.save(os.path.join(args.outpath, 'trajectory.freiburg'))
+            wandb.save(os.path.join(args.outpath, 'trajectory.json'))
+            wandb.save(os.path.join(args.outpath, 'map.ply'))
+            if os.path.isfile(os.path.join(args.input, 'groundtruth.txt')):
+                error = evaluate(os.path.join(args.input, 'groundtruth.txt'),
+                                 os.path.join(args.outpath, 'trajectory.freiburg'))
                 wandb.define_metric('trans_error', step_metric='frame')
                 for i, e in enumerate(error):
                     wandb.log({'trans_error': e, 'frame': i})
@@ -175,6 +173,17 @@ if __name__ == '__main__':
         action="store_true",
         help='store intermediate maps'
     )
+    parser.add_argument(
+        '--viewer',
+        default='none',
+        choices=['none', '2d', '3d'],
+        help='select viewer'
+    )
+    parser.add_argument(
+        '--block_viewer',
+        action="store_true",
+        help='block viewer if viewer selected.'
+    )
     args = parser.parse_args()
     with open(args.config, 'r') as ymlfile:
         config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
@@ -182,4 +191,4 @@ if __name__ == '__main__':
         args.outpath = os.path.join(args.input, 'data','alleyoop')
     assert os.path.isfile(args.checkpoint), 'no valid checkpoint file'
 
-    main(args.input, args.outpath, config, args.device, args.stop, args.start, args.step, args.log, args.force_video, args.checkpoint, args.store_map)
+    main(args, config)
