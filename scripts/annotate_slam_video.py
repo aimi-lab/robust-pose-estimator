df3742f46d0baa47e27f879fbb7dd801b93e423d
mhayoz <mi.hayoz@sunrise.ch>
2022-10-17 16:10:23 +0200

    make this parallel

--- scripts/annotate_slam_video.py
+++ scripts/annotate_slam_video.py
@@ -6,6 +6,7 @@ import cv2
 
 from alley_oop.utils.trajectory import read_freiburg
 from alley_oop.network_core.raft.core.datasets import RGBDecoder
+from tqdm import tqdm
 
 def annotate(root):
     rgb_decoder = RGBDecoder()
@@ -21,7 +22,7 @@ def annotate(root):
     assert len(semantics) == len(images_l)
 
     annotations = []
-    for i in range(len(images_l)-1):
+    for i in tqdm(range(len(images_l)-1), total=len(images_l)):
         camera_motion = np.linalg.norm(np.linalg.inv(poses[i+1].astype(np.float64)) @ poses[i].astype(np.float64) - np.eye(4)) > 0.01
         tool_present = np.mean(_read_mask(semantics[i])) < 0.95
         annotations.append((camera_motion, tool_present))
@@ -31,13 +32,17 @@ def annotate(root):
         wr.writerows(annotations)
 
 
-PATH_REPLACEMENT = ['/home/mhayoz/research', '/storage/workspaces/artorg_aimi/ws_00000']
 if __name__ == '__main__':
-    with open(os.path.join('..', 'intuitive_slam_train.txt'), 'r') as f:
-        sequences = f.readlines()
-    for sequence in sequences:
-        print(sequence)
-        sequence = sequence.replace(PATH_REPLACEMENT[1], PATH_REPLACEMENT[0])
-        sequence = sequence.replace('\n', '')
-        if not os.path.isfile(os.path.join(sequence, 'annotions.csv')):
-            annotate(sequence)
+    import argparse
+    parser = argparse.ArgumentParser(description='script to annotate video for camera motion and tool presence')
+
+    parser.add_argument(
+        'input',
+        type=str,
+        help='Path to input folder.'
+    )
+    args = parser.parse_args()
+
+    print(args.input)
+    if not os.path.isfile(os.path.join(args.input, 'annotions.csv')):
+        annotate(args.input)
