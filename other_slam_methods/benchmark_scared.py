92f9a61dcff3b8718a8818556ada0ae62d66649a
mhayoz <mi.hayoz@sunrise.ch>
2022-10-27 10:50:52 +0200

    update path

--- other_slam_methods/benchmark_scared.py
+++ other_slam_methods/benchmark_scared.py
@@ -17,16 +17,16 @@ if __name__ == '__main__':
         sequence = sequence.replace(PATH_REPLACEMENT[1], PATH_REPLACEMENT[0])
         sequence = sequence.replace('\n', '')
         try:
-            if not os.path.isfile(os.path.join(sequence, 'data/efusion/trajectory.json')):
+            if not os.path.isfile(os.path.join(sequence, 'data/efusion/trajectory.freiburg')):
                 with open('configuration/efusion_scared.yaml', 'r') as ymlfile:
                     config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
                 p = Process(target=elfusion, args=(sequence, os.path.join(sequence, 'data/efusion'), config, 'cpu', 0, 10000000, 1, 'scared_efusion', False,))
                 p.start()
                 p.join()
-            if not os.path.isfile(os.path.join(sequence, 'data/orbslam2/trajectory.json')):
+            if not os.path.isfile(os.path.join(sequence, 'data/orbslam2/trajectory.freiburg')):
                 with open('configuration/orbslam2.yaml', 'r') as ymlfile:
                     config = yaml.load(ymlfile, Loader=yaml.SafeLoader)
-                p = Process(target=orbslam, args=(sequence, os.path.join(sequence, 'data/orbslam2'), config, 'cpu', 0, 1000000000, 1, 'scared_orbslam2', False,))
+                p = Process(target=orbslam, args=(sequence, os.path.join(sequence, 'data/orbslam2'), config, 'cpu', 0, 1000000000, 1, 'orbslam2_raftdepth', False,))
                 p.start()
                 p.join()
         except:
