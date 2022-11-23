63f992fe26fb91f37028d61d2222d12616f5b837
mhayoz <mi.hayoz@sunrise.ch>
2022-11-23 09:24:00 +0100

    remove depreciated tests

--- tests/unit_test_all.py
+++ tests/unit_test_all.py
@@ -1,16 +1,7 @@
-from tests.unit_test_normals import NormalsTester
-from tests.unit_test_photo_loss import PhotoLossTest
-from tests.unit_test_quaternions import QuaternionConversionTester
 from tests.unit_test_lie import Lie3DTester
-from tests.unit_test_gauss_pyramid import GaussPyramidTester
-from tests.unit_test_grayscale import GrayscaleTester
-from tests.unit_test_surfel_map import SurfelMapTest
-from tests.unit_test_pose_head import PoseHeadTester
 
 test_classes = [
-    NormalsTester, PhotoLossTest, Lie3DTester, QuaternionConversionTester,
-    GaussPyramidTester, GrayscaleTester, SurfelMapTest,
-    PoseHeadTester
+    Lie3DTester
     ]
 
 for test_class in test_classes:
