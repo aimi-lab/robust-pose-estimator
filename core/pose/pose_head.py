52cb45be8f19c8d3566565271fd7fc9b26379e04
mhayoz <mi.hayoz@sunrise.ch>
2022-12-01 15:06:12 +0100

    remove line-search for better convergence with SE3

--- core/pose/pose_head.py
+++ core/pose/pose_head.py
@@ -65,7 +65,8 @@ class DeclarativePoseHead3DNode(AbstractDeclarativeNode):
             n = xs[0].shape[0]
             # Solve using LBFGS optimizer:
             y = LieGroupParameter(SE3.Identity(n, device=xs[0].device, requires_grad=True, dtype=torch.float64))
-            optimizer = torch.optim.LBFGS([y], lr=0.5, max_iter=self.lbgfs_iters, line_search_fn="strong_wolfe", )
+            # don't use strong-wolfe with lietorch SE3, it does not converge
+            optimizer = torch.optim.LBFGS([y], lr=1.0, max_iter=self.lbgfs_iters, line_search_fn=None, )
 
             def fun():
                 optimizer.zero_grad()
